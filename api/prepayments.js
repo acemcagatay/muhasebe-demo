4a38e0f0fbc4307efebc8538d450240910f1e1c8
acemcagatay <acemcagatay@gmail.com>
2026-03-09 19:36:59 +0300

    Create prepayments.js

---
+++ api/prepayments.js
@@ -0,0 +1,38 @@
+import fs from "fs";
+import path from "path";
+
+const filePath = path.join(process.cwd(), "data.json");
+
+export default function handler(req, res) {
+
+  if (req.method === "POST") {
+
+    const newData = req.body;
+
+    let data = [];
+
+    if (fs.existsSync(filePath)) {
+      data = JSON.parse(fs.readFileSync(filePath));
+    }
+
+    data.push(newData);
+
+    fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
+
+    return res.status(200).json({ success: true });
+
+  }
+
+  if (req.method === "GET") {
+
+    if (!fs.existsSync(filePath)) {
+      return res.status(200).json([]);
+    }
+
+    const data = JSON.parse(fs.readFileSync(filePath));
+
+    return res.status(200).json(data);
+
+  }
+
+}
